12ef1e2689a523e957eadb21c928444606d30e60
3n3a <46775561+3n3a@users.noreply.github.com>
2022-06-14 17:10:49 +0200

    Add synonyms for substances table

--- application/db/init.sql
+++ application/db/init.sql
@@ -12,6 +12,11 @@ CREATE TABLE IF NOT EXISTS images (
     substance_id INT REFERENCES substances(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS synonyms (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
 CREATE TABLE IF NOT EXISTS groups (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE
